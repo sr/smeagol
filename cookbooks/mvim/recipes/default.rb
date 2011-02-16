ffbb233e745a12f5bd43c2e7119811e0628578bb
Simon Rozet <simon@rozet.name>
2011-02-16 13:18:55 -0800

    extract vim cookbook out of mvim

--- cookbooks/mvim/recipes/default.rb
+++ cookbooks/mvim/recipes/default.rb
@@ -3,34 +3,7 @@
 # Recipe:: default
 #
 
-script "installing http://github.com/carlhuda/janus" do
-  interpreter "bash"
-  code <<-EOS
-    source ~/.cinderella.profile
-    if [ ! -d ~/.vim ]; then
-      git clone git://github.com/carlhuda/janus.git ~/.vim
-      cd ~/.vim
-      rake
-    elif [ ! -d ~/.vim/Rakefile ]; then
-      for i in ~/.vim ~/.vimrc ~/.gvimrc; do [ -e $i ] && mv $i $i.old; done
-      git clone git://github.com/carlhuda/janus.git ~/.vim
-      cd ~/.vim
-      rake
-    fi
-  EOS
-end
-
-directory "#{ENV['HOME']}/.vimswap" do
-  action 'create'
-end
-
-template "#{ENV['HOME']}/.vimrc.local" do
-  source "dot.vimrc.erb"
-end
-
-template "#{ENV['HOME']}/.gvimrc.local" do
-  source "dot.vimrc.erb"
-end
+include_recipe "vim"
 
 script "installed macvim from google code" do
   interpreter "bash"
