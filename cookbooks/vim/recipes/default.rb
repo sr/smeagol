145e628cc3df49d11857ef501f59407703e167ad
Simon Rozet <simon@rozet.name>
2011-02-22 16:52:46 -0800

    allow config to set janus fork and ref

--- cookbooks/vim/recipes/default.rb
+++ cookbooks/vim/recipes/default.rb
@@ -1,17 +1,20 @@
 script "installing http://github.com/carlhuda/janus" do
+  repo  = ENV["JANUS_REPO"] || "git://github.com/carlhuda/janus.git"
+  ref   = ENV["JANUS_REF"]  || "origin/master"
+
   interpreter "bash"
   code <<-EOS
     source ~/.cinderella.profile
     if [ ! -d ~/.vim ]; then
-      git clone git://github.com/sr/janus.git ~/.vim
-      git reset --hard origin/sr
+      git clone #{repo} ~/.vim
       cd ~/.vim
+      git reset --hard #{ref}
       rake
     elif [ ! -d ~/.vim/Rakefile ]; then
       for i in ~/.vim ~/.vimrc ~/.gvimrc; do [ -e $i ] && mv $i $i.old; done
-      git clone git://github.com/sr/janus.git ~/.vim
+      git clone #{repo} ~/.vim
       cd ~/.vim
-      git reset --hard origin/sr
+      git reset --hard #{ref}
       rake
     fi
   EOS
